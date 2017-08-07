b4cc67d6ee68cfbdaf73f7b017c1fb83e951bbbb
Naoto Satoh <stardustforever012@gmail.com>
2017-08-08 01:50:01 +0900

    first commit

---
+++ mnemonic_search.js
@@ -0,0 +1,19 @@
+var bip39 = require('./bip39');
+var wordlist = bip39.wordlists.EN; 
+
+var my_mnemonic='input mnemonic words';
+
+for(var j=0; j < 24; j++){
+  var index = j;
+  var mnemonic_arr = my_mnemonic.split(/\s/);
+
+  for(var i=0; i< 2048; i++){
+    mnemonic_arr[index] = wordlist[i];
+    var mnemonic = mnemonic_arr.join(' ');
+    var result = bip39.validateMnemonic(mnemonic, wordlist);
+
+    if(result){
+      console.log(mnemonic);
+    }
+  }
+}
